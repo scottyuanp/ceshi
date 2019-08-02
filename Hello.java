7861d6c45990aa0e77ac5300b76d0c5f47226086
scottyuanp <2321030843@qq.com>
2019-08-02 22:20:54 +0800

    Update Hello.java Fie . Add Two Lines

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		System.out.println("www.mldn.com");
+		System.out.println("www.jd.com");
 	}
 	
 }
